7739648622753bfeec4da9178140d58f35e03c64
itsmeyounha <shofura.adzani@gmail.com>
2015-03-23 18:12:44 +0700

    k

--- MenuUtama.java
+++ MenuUtama.java
@@ -40,5 +40,28 @@ public class MenuUtama extends World
         jalanan2.setLocation(220, 0);
         jalanan3.setLocation(220, 600);
         jalanan.setLocation(220, 300);
+        TombolStart tombolstart = new TombolStart();
+        addObject(tombolstart, 626, 148);
+        TombolHowtoPLay tombolhowtoplay = new TombolHowtoPLay();
+        addObject(tombolhowtoplay, 634, 263);
+        tombolhowtoplay.setLocation(626, 258);
+        TombolCredit tombolcredit = new TombolCredit();
+        addObject(tombolcredit, 631, 374);
+        tombolcredit.setLocation(627, 366);
+        TombolExit tombolexit = new TombolExit();
+        addObject(tombolexit, 635, 482);
+        tombolexit.setLocation(628, 475);
+        tombolstart.setLocation(626, 154);
+        tombolexit.setLocation(629, 538);
+        tombolexit.setLocation(628, 520);
+        tombolcredit.setLocation(628, 414);
+        tombolhowtoplay.setLocation(628, 309);
+        tombolstart.setLocation(628, 211);
+        tombolcredit.setLocation(630, 409);
+        tombolstart.getX();
+        tombolstart.setLocation(620, 200);
+        tombolhowtoplay.setLocation(620, 300);
+        tombolcredit.setLocation(620, 400);
+        tombolexit.setLocation(620, 500);
     }
 }
--- TombolMenuUtama.java
+++ TombolMenuUtama.java
@@ -12,8 +12,26 @@ public class TombolMenuUtama extends Actor
      * Act - do whatever the TombolMainMenu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    
     public void act() 
     {
         // Add your action code here.
-    }    
+         
+    } 
+    
+    public void TombolHover()
+    {
+        //MouseInfo mouseOver = Greenfoot.getMouseInfo();  
+        GreenfootImage image = getImage();
+        boolean mouseOver = false;
+        
+       if(Greenfoot.mouseMoved(null))
+        mouseOver = Greenfoot.mouseMoved(this);
+        
+       if(mouseOver) 
+        image.scale(350,125);
+       else if(!Greenfoot.mouseMoved(this))
+        image.scale(325,100);
+    }
 }
