5f5dbb1c2d3d26a541043c24657131154023cbb5
jejewanda <jessikawanda24@gmail.com>
2015-03-29 23:50:54 +0700

    jejee

--- Game.java
+++ Game.java
@@ -22,6 +22,7 @@ public class Game extends World
     private int faktor = 35;
     private int active = 0;
     private Countdown down;
+    int ct = 0;
     
     Polisi polisi;
     Maling maling;
@@ -39,7 +40,7 @@ public class Game extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1);
-        setPaintOrder(Countdown.class,Peluru.class,Rintangan.class,Polisi.class,Maling.class,Jalanan.class);
+        setPaintOrder(Countdown.class,Peluru.class,Rintangan.class,TambahKecepatan.class,Polisi.class,Maling.class,Jalanan.class);
         character = i;
         if (i==1) {
             speed = 4;
@@ -115,18 +116,27 @@ public class Game extends World
             /* Rintangan
              *   buat logika munculin rintangan secara random disini
              */
-            int ct = 0;
+            
             ct++;
-            if(ct == 10)
+            if(ct == 150)
             {
-                int pil = Greenfoot.getRandomNumber(2);
-                int x = 250 + Greenfoot.getRandomNumber(100);
-                int y = 50 + Greenfoot.getRandomNumber(100);
-                if(pil == 1)
-                    addObject(new Rintangan(), x, y);
+                int pil = Greenfoot.getRandomNumber(5);
+                int x = 272 + Greenfoot.getRandomNumber(253);
+                int y = 100;
+                if(pil == 0)
+                {
+                    addObject(new oli(speed), x, y);
+                }
+                else if(pil == 1)
+                {
+                    addObject(new lubang(speed), x, y);
+                }
                 else if(pil == 2)
-                    addObject(new Rintangan(), x, y);
+                {
+                    addObject(new petir(speed), x, y);
+                }
                 ct = 0;
+                
             }
             /* Dekorasi
              *   buat logika munculin dekorasi disamping jalan secara random disini
